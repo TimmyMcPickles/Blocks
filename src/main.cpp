942545f2b930e993d79b285ed02e90ae647d4632
Ethan <kingscout1011@gmail.com>
2025-02-08 21:47:17 -0600

    I'm batman

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,86 @@
 #include "raylib.h"
+#include <cmath>
 #include <iostream>
 
-#pragma region imgui
-#include "imgui.h"
-#include "rlImGui.h"
-#include "imguiThemes.h"
-#pragma endregion
+//#pragma region imgui
+//#include "../thirdparty/imgui-docking/imgui/imgui.h"
+//#include "../thirdparty/rlImGui/rlImGui.h"
+//#include "../thirdparty/imgui-docking/imgui/imguiThemes.h"
+//#pragma endregion
 
-
+typedef enum gameState{
+    START = 0,
+    PAUSE = 1,
+    END = 2
+} gameState;
 
 int main(void)
 {
+    // Initialization
+    int screenWidth = GetScreenWidth();
+    int screenHeight = GetScreenHeight();
 
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-	InitWindow(800, 450, "raylib [core] example - basic window");
+	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-#pragma region imgui
-	rlImGuiSetup(true);
+	gameState currentState = START;
+	int frameCounter = 0;
 
-	//you can use whatever imgui theme you like!
-	//ImGui::StyleColorsDark();
-	//imguiThemes::yellow();
-	//imguiThemes::gray();
-	imguiThemes::green();
-	//imguiThemes::red();
-	//imguiThemes::embraceTheDarkness();
-
-
-	ImGuiIO &io = ImGui::GetIO(); (void)io;
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-	io.FontGlobalScale = 2;
-
-	ImGuiStyle &style = ImGui::GetStyle();
-	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-	{
-		//style.WindowRounding = 0.0f;
-		style.Colors[ImGuiCol_WindowBg].w = 0.5f;
-		//style.Colors[ImGuiCol_DockingEmptyBg].w = 0.f;
-	}
-
-#pragma endregion
-
-
-
+	SetTargetFPS(60);
+	// Main loop
 	while (!WindowShouldClose())
 	{
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-
-
-	#pragma region imgui
-		rlImGuiBegin();
-
-		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
-		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
-		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-		ImGui::PopStyleColor(2);
-	#pragma endregion
-
-
-		ImGui::Begin("Test");
-
-		ImGui::Text("Hello");
-		ImGui::Button("Button");
-		ImGui::Button("Button2");
-
-		ImGui::End();
-
-
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-
-	#pragma region imgui
-		rlImGuiEnd();
-
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	    // Update
+	switch (currentState)
+	{
+		case START:
 		{
-			ImGui::UpdatePlatformWindows();
-			ImGui::RenderPlatformWindowsDefault();
-		}
-	#pragma endregion
+		  if(IsKeyPressed(KEY_SPACE))
+			{
+			    currentState = PAUSE;
+			}
+		}break;
 
-		EndDrawing();
+		case PAUSE:
+		{
+			 if (IsKeyPressed(KEY_SPACE)){
+				currentState = START;
+			}
+		}break;
+		case END:
+		{
+		    if (IsKeyPressed(KEY_ESCAPE)){
+						currentState = END;
+						}
+		}break;
+		default: break;
 	}
 
+		//Draw
+	BeginDrawing();
+	   ClearBackground(WHITE);
 
-#pragma region imgui
-	rlImGuiShutdown();
-#pragma endregion
-
-
-
+		switch(currentState){
+		case START:
+		  {
+		    DrawText("Title screen", 20, 20, 40, BLACK);
+			DrawText("Press [SPACE] to pause, and [ESC] to end.", 290, 220, 20, BLACK);
+		  }break;
+		case PAUSE:
+		  {
+			DrawRectangle(0,0,screenWidth, screenHeight, GRAY);
+			DrawText("PAUSE",20, 20, 40, RED);
+		  }break;
+		case END:
+		  {
+			DrawRectangle(0, 0, screenWidth, screenHeight, GRAY);
+			DrawText("END!!!", 20, 20, 40, WHITE);
+		  }break;
+		default: break;
+		}
+		EndDrawing();
+	}
+	// De-Initialization
 	CloseWindow();
 
 	return 0;
